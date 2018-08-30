668a561e330be871e9277099b87b74a8aadd9fd2
kosay88 <kosay.faraj.88@gmail.com>
2018-08-30 12:37:03 +0200

    fix bugs 2

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MapContainer from './components/Map';
 import Login from './components/Login';
 import AreaImg from './components/AreaImg';
-<<<<<<< HEAD
 import Register from './components/AdminRegister';
 import AdminNav from './components/AdminNav';
 import AdminLogin from './components/AdminLogin';
 import DashboardArea from './components/AdminDashboardArea';
 import AddPhoto from './components/AddPhoto';
-=======
->>>>>>> 176deedafef5bf3051124ce8528174912722bb23
 
 class App extends Component {
   render() {
@@ -24,14 +21,12 @@ class App extends Component {
           <Route path="/mainpage" component={Main} />
           <Route path="/Login" component={Login}/>
           <Route path="/AreaImg/:id" component={AreaImg}/>
-<<<<<<< HEAD
+
           <Route path='/Admin-panel/Register' component={Register} />
           <Route path='/Adminlogin' component={AdminLogin} />
           <Route path='/Admin-panel/DashboardAreas' component={DashboardArea} />
           <Route path='/Addphoto' component={AddPhoto} />
 
-=======
->>>>>>> 176deedafef5bf3051124ce8528174912722bb23
         </Switch>
       </Router>
     );
